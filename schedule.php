6f7fbb4d78303b82d2f73d384bd2b8aee39d59b8
Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
2022-09-20 10:19:31 +0100

    Added Current Subject & Fixed Errors

--- schedule.php
+++ schedule.php
@@ -17,7 +17,7 @@
       h1, h3, select, form {
         text-align: center;
       }
-      select {
+      select, button {
         display: block;
         margin: 0 auto;
       }
@@ -25,11 +25,11 @@
     <?php 
         $schedule = array(
             "Time" => array("08:00-08:55", "08:55-09:50", "09:50-10:45", "10:45-11:15", "11:15-12:10", "12:10-13:05", "13:05-14:00"),
-            "L" => array("EMR", "DSW", "DSW", "R", "DEW", "DEW", "DEW"),
-            "M" => array("DPL", "DPL", "DSW", "E", "DSW", "DOR", "DOR"),
-            "X" => array("DEW", "DEW", "DSW", "CR", "DSW", "DOR", "DOR"),
-            "J" => array("DPL", "DPL", "DPL", "E", "DEW", "DEW", "EMR"),
-            "V" => array("DOR", "DOR", "DPL", "O", "EMR", "DSW", "DSW")
+            "Mo" => array("EMR", "DSW", "DSW", "R", "DEW", "DEW", "DEW"),
+            "Tu" => array("DPL", "DPL", "DSW", "E", "DSW", "DOR", "DOR"),
+            "We" => array("DEW", "DEW", "DSW", "CR", "DSW", "DOR", "DOR"),
+            "Th" => array("DPL", "DPL", "DPL", "E", "DEW", "DEW", "EMR"),
+            "Fr" => array("DOR", "DOR", "DPL", "O", "EMR", "DSW", "DSW")
         );
         
         $legend = array(
@@ -65,37 +65,53 @@
             <?php }
         }
 
-        function findSubject(){
+        function findSubject($dayA, $hourA, $minutesA){
           global $schedule;
-          $dayA = $_POST['day'];
-          $hourA = $_POST['hour'];
-          $minutesA = $_POST['minutes'];
+          $flag = false;
+          if(is_null($dayA)){
+            date_default_timezone_set("Europe/London");
+            $dayA = substr(date(DATE_RFC850), 0, 2);
+            $hourA = date("h");
+            $minutesA = date("i");
+            $flag = true;
+          }
           $class;
 
           if($hourA == 8 && $minutesA < 55) $class = "0";
-          if($hourA == 8 && $minutesA > 55 || $hourA == 9 && $minutesA < 50) $class = "1";
-          if($hourA == 9 && $minutesA > 50 || $hourA == 10 && $minutesA < 45) $class = "2";
-          if($hourA == 10 && $minutesA > 45 || $hourA == 11 && $minutesA < 15) $class = "6";
-          if($hourA == 11 && $minutesA > 15 || $hourA == 12 && $minutesA < 10) $class = "3";
-          if($hourA == 12 && $minutesA > 10 || $hourA == 13 && $minutesA < 05) $class = "4";
-          if($hourA == 13 && $minutesA > 05) $class = "5";
+          if($hourA == 8 && $minutesA >= 55 || $hourA == 9 && $minutesA < 50) $class = "1";
+          if($hourA == 9 && $minutesA >= 50 || $hourA == 10 && $minutesA < 45) $class = "2";
+          if($hourA == 10 && $minutesA >= 45 || $hourA == 11 && $minutesA < 15) $class = "7";
+          if($hourA == 11 && $minutesA >= 15 || $hourA == 12 && $minutesA < 10) $class = "4";
+          if($hourA == 12 && $minutesA >= 10 || $hourA == 13 && $minutesA < 05) $class = "5";
+          if($hourA == 13 && $minutesA >= 05) $class = "6";
 
-          if($class == "6"){
-            echo "<br/><table><tr><td>¡Toca recreo!</td></tr></table>";
+          if($class == "7"){
+            if($flag) echo "<br/><table><tr><td>¡Ahora toca recreo!</td></tr></table>";
+            else echo "<br/><table><tr><td>¡Toca recreo!</td></tr></table>";
             return;
           }
 
           for($i = 1; $i < 7; $i++){
             foreach($schedule as $day => $subjects){
               if($day != $dayA) continue;
-              echo "<h3 style='margin-left:auto; margin-right:auto; width:30%; background-color: lightblue;'>Toca: {$subjects[$class]}</h3>";
+              if($flag) echo "<h3 style='margin-left:auto; margin-right:auto; width:30%; background-color: lightblue;'>Ahora Toca: {$subjects[$class]}</h3>";
+              else {
+                if($day == "Mo") $dayS = "Lunes";
+                if($day == "Tu") $dayS = "Martes";
+                if($day == "We") $dayS = "Miércoles";
+                if($day == "Th") $dayS = "Jueves";
+                if($day == "Fr") $dayS = "Viernes";
+                if($minutesA < 10) {
+                  $minutes = "0{$minutesA}";
+                  echo "<h3 style='margin-left:auto; margin-right:auto; width:30%; background-color: lightblue;'>El {$dayS} a las {$hourA}:{$minutes} Toca {$subjects[$class]}</h3>";
+                }else echo "<h3 style='margin-left:auto; margin-right:auto; width:30%; background-color: lightblue;'>El {$dayS} a las {$hourA}:{$minutesA} Toca {$subjects[$class]}</h3>";
+              }
               return;
             }
           }
         }
     ?>
     <h1>Horario 2ºDAW - Mañana</h1>
-    <br/>
     <table>
       <tr>
         <th></th>
@@ -117,22 +133,22 @@
       </tr>  
       <?php printLegend()?>
     </table>
-    <h3>Buscar qué toca:</h3>
-    <select id="day" name="day" form="form">
-      <option value="L">Lunes</option>
-      <option value="M">Martes</option>
-      <option value="X">Miércoles</option>
-      <option value="J">Jueves</option>
-      <option value="V">Viernes</option>
+    <br/>
+    <?php findSubject(null, null, null);?>
+    <h3>Buscar qué toca eligiendo día y hora:</h3>
+    <select id="day" name="day" form="formA">
+      <option value="Mo">Lunes</option>
+      <option value="Tu">Martes</option>
+      <option value="We">Miércoles</option>
+      <option value="Th">Jueves</option>
+      <option value="Fr">Viernes</option>
     </select>
     <br/>
-    <form action="ut1.3.php" id="form" method="POST">
+    <form action="ut1.3.php" id="formA" method="POST">
       <input type="number" maxlength="2" required="true" onKeyDown="return false" id="hour" name="hour" placeholder="Hora" min="8" max="13">
       <input type="number" maxlength="2" default="0" onKeyDown="return false" id="minutes" name="minutes" placeholder="Minutos" min="0" max="59">
       <input type="submit">
     </form>  
-    <?php
-      if(count($_POST)) findSubject();
-    ?>
+    <?php if(count($_POST)) findSubject($_POST['day'], $_POST['hour'], $_POST['minutes']);?>
   </body>
 </html>
